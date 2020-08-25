0d2a4b452720d10baabd6bd41f8531f4f2f91b3d
Simon Herlöfsson <simon.herlofsson@svenskaspel.se>
2020-08-25 23:19:10 +0200

    fixes

--- jobs/app.py
+++ jobs/app.py
@@ -14,12 +14,13 @@ def open_connection():
     return connection
 
 def execute_sql(sql, values = (), commit = False, single = False):
-    connection = open_connection
+    connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
     return results
 
 @app.teardown_appcontext
